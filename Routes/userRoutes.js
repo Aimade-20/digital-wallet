ae098dc100d741d539e61476b1c72b7f46b471ef
Aimade safsafi <imadesafsafi8@gmail.com>
2026-02-19 15:29:36 +0000

    fist com wallet

---
+++ Routes/userRoutes.js
@@ -0,0 +1,30 @@
+const {
+  getAllUsers,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require("../Controllers/userController");
+
+function userRoutes(req, res) {
+  if (req.method === "GET" && req.url === "/users") {
+    return getAllUsers(req, res);
+  }
+
+  if (req.method === "POST" && req.url === "/users") {
+    return createUser(req, res);
+  }
+
+  if (req.method === "PUT" && req.url.startsWith("/users/")) {
+    const id = req.url.split("/")[2];
+    return updateUser(req, res, id);
+  }
+
+  if (req.method === "DELETE" && req.url.startsWith("/users/")) {
+    const id = req.url.split("/")[2];
+    return deleteUser(req, res, id);
+  }
+
+  return null;
+}
+
+module.exports = userRoutes;
